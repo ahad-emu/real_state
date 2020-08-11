ba4e3eb61b4d1f946867f18cced73a09975b3378
ahad-emu <ahademu99@gmail.com>
2020-08-11 19:55:37 +0600

    search option done

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from listings.models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from listings.choices import bedroom_choices, state_choices, price_choices
 
 # Create your views here.
 def index(request):
@@ -10,11 +11,59 @@ def index(request):
     page = request.GET.get('page')
     paged_item = paginator.get_page(page)
 
-    return render(request, "listings/listings.html", {'listings': paged_item})
+    context = {
+        'listings': paged_item ,
+    }
+
+    return render(request, "listings/listings.html", context)
 
 def listing(request, Listing_id):
     item = Listing.objects.get(pk = Listing_id)
-    return render(request, "listings/listing.html" , {'item' : item })
+
+    context = {
+        'item': item ,
+    }
+
+    return render(request, "listings/listing.html" , context)
 
 def search(request):
-    return render(request, "listings/search.html")
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    #city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__iexact=bedrooms)
+
+    context = {
+        'bedroom_choices' : bedroom_choices ,
+        'state_choices' : state_choices ,
+        'price_choices' : price_choices ,
+        'listings' : queryset_list,
+    }
+    return render(request, "listings/search.html", context)
--- real_state_app/views.py
+++ real_state_app/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import bedroom_choices, state_choices, price_choices
 
 # Create your views here.
 def home(request):
     items = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
-    return render(request, "pages/home.html", {'items': items})
+    context = {
+        'items' : items ,
+        'bedroom_choices' : bedroom_choices ,
+        'price_choices' : price_choices ,
+        'state_choices' : state_choices ,
+    }
+
+    return render(request, "pages/home.html", context)
 
 
 def about(request):
     realtors = Realtor.objects.order_by('-hired_date')
-
     mvp_realtors = Realtor.objects.filter(is_mvp=True)
 
-    return render(request, "pages/about.html", {'realtors': realtors, 'mvp_realtors': mvp_realtors})
+    context = {
+        'realtors' : realtors ,
+        'mvp_realtors' : mvp_realtors ,
+    }
+
+    return render(request, "pages/about.html", context)
